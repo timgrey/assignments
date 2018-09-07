66edb4d300def50c231cb92c983fb8ecbf12be59
Tim <timothy.a.grey@gmail.com>
2018-09-07 10:47:25 -0600

    Assignment Submission

--- exercises/exercise-loops/app.js
+++ exercises/exercise-loops/app.js
@@ -1,4 +1,4 @@
-Function 1
+//Function 1
 
 function stringLetters(string) {
     for (var i=0; i<string.length; i++) {
@@ -7,7 +7,7 @@ function stringLetters(string) {
 }
 stringLetters(" Hello ");
 
-Function 2
+//Function 2
 
 function characterPosition(string, character) {
     var index = string.indexOf(character)
@@ -21,7 +21,7 @@ function characterPosition(string, character) {
 console.log(characterPosition("hello", "o"));
 console.log(characterPosition("hello", "x"));
 
-Function 3
+//Function 3
 
 function fortyTwo(array) {
     for (var i=0; i<array.length; i++) {
@@ -36,7 +36,7 @@ function fortyTwo(array) {
 console.log(fortyTwo([2,3,4,5,6,4,3,3,42]))
 console.log(fortyTwo([2,3,4,5,6,4,3,3,3]))
 
-Function 4
+//Function 4
 
 function smallestNum(array) {
     var min = array[0]
