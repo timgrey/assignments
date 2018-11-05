7b0a893dccbde3954843926c6be4aeac35fb33b1
Tim <timothy.a.grey@gmail.com>
2018-11-05 10:23:39 -0700

    edited for deployment

--- portfolio/react-weather-app/server.js
+++ portfolio/react-weather-app/server.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const mongoose = require("mongoose")
 require("dotenv").config();
+const path = require("path")
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 app.use(express.json());
 app.use("/api/users", require("./routes/users"));
 
@@ -15,6 +19,12 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true}, () => {
     console.log("Connected to MongoDB")
 })
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+})
+
 app.listen(process.env.PORT, () => {
     console.log("Connected on port " + process.env.PORT)
 })
+
+
